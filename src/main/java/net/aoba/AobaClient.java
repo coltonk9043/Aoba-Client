969b0cf22c6dc9ae7cdb1e4be923ce5733845292
Colton John Kennedy <ckenne87@uwo.ca>
2023-06-10 09:38:30 -0400

    Fixed description

--- src/main/java/net/aoba/AobaClient.java
+++ src/main/java/net/aoba/AobaClient.java
@@ -34,7 +34,7 @@ import net.minecraft.client.util.math.MatrixStack;
 
 public class AobaClient {
 	public static final String NAME = "Aoba";
-	public static final String VERSION = "1.19.4";
+	public static final String VERSION = "1.20";
 	public static final String PREFIX = ".aoba";
 
 	public static MinecraftClient MC;
