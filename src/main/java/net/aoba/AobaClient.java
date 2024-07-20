4e94b3a2b0ee31e1f8f87b6989ddb738b4745790
cvs0 <cvs0@lockscript.dev>
2024-07-20 17:27:40 -0600

    bumped aoba version

--- src/main/java/net/aoba/AobaClient.java
+++ src/main/java/net/aoba/AobaClient.java
@@ -45,7 +45,7 @@ import java.util.List;
 public class AobaClient {
     public static final String NAME = "Aoba";
     public static final String VERSION = "1.21";
-    public static final String AOBA_VERSION = "1.4.2";
+    public static final String AOBA_VERSION = "1.4.3";
 
     public static MinecraftClient MC;
     public static IMinecraftClient IMC;
