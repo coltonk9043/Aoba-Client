31e58e55f2c6277e6a106e69ce7d33cd3c06db3a
cvs0 <cvs0@lockscript.dev>
2024-07-23 23:43:55 -0600

    Implicitly cast int multiplication to long

--- src/main/java/net/aoba/misc/ModuleUtils.java
+++ src/main/java/net/aoba/misc/ModuleUtils.java
@@ -68,7 +68,7 @@ public class ModuleUtils {
 
                     return new ChunkPos(x, z);
 
-                }).limit(diameter * diameter)
+                }).limit((long) diameter * diameter)
                 .filter(c -> AobaClient.MC.world.isChunkLoaded(c.x, c.z))
                 .map(c -> AobaClient.MC.world.getChunk(c.x, c.z)).filter(Objects::nonNull);
 
