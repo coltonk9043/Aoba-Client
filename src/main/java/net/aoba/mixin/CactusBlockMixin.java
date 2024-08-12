f1e342e8c2af192020381fcf0039eb2ce99413de
Colton <coltonk9043@yahoo.ca>
2024-08-12 12:48:05 -0400

    Update CactusBlockMixin.java

--- src/main/java/net/aoba/mixin/CactusBlockMixin.java
+++ src/main/java/net/aoba/mixin/CactusBlockMixin.java
@@ -43,7 +43,7 @@ public abstract class CactusBlockMixin extends Block {
             "getCollisionShape(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/ShapeContext;)Lnet/minecraft/util/shape/VoxelShape;"}, cancellable = true)
     private void onGetCollisionShape(BlockState blockState_1, BlockView blockView_1, BlockPos blockPos_1,
                                      ShapeContext entityContext_1, CallbackInfoReturnable<VoxelShape> cir) {
-        if (Aoba.getInstance() != null) {
+        if (Aoba.getInstance() != null && Aoba.getInstance().moduleManager != null) {
             if (Aoba.getInstance().moduleManager.anticactus.getState()) {
                 cir.setReturnValue(VoxelShapes.fullCube());
             }
