d96c396a42114e3b2dc74444f947928147cd780b
Colton <coltonk9043@yahoo.ca>
2024-08-17 16:09:04 -0400

    Better EnumComponent

--- src/main/java/net/aoba/gui/components/EnumComponent.java
+++ src/main/java/net/aoba/gui/components/EnumComponent.java
@@ -1,46 +1,32 @@
 package net.aoba.gui.components;
 
-import net.aoba.Aoba;
-import net.aoba.event.events.KeyDownEvent;
 import net.aoba.event.events.MouseClickEvent;
-import net.aoba.event.listeners.KeyDownListener;
+import net.aoba.event.events.MouseMoveEvent;
+import net.aoba.gui.GuiManager;
 import net.aoba.gui.IGuiElement;
 import net.aoba.gui.Margin;
 import net.aoba.gui.Rectangle;
 import net.aoba.gui.colors.Color;
 import net.aoba.settings.types.EnumSetting;
+import net.aoba.utils.input.CursorStyle;
 import net.aoba.utils.render.Render2D;
 import net.aoba.utils.types.MouseAction;
 import net.aoba.utils.types.MouseButton;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.util.math.MatrixStack;
-import org.joml.Matrix4f;
-import org.lwjgl.glfw.GLFW;
+import net.minecraft.util.Colors;
 
-public class EnumComponent<T extends Enum<T>> extends Component implements KeyDownListener {
-	private boolean listeningForKey;
+
+public class EnumComponent<T extends Enum<T>> extends Component {
 	private EnumSetting<T> enumSetting;
-	private boolean isFocused = false;
-	private float focusAnimationProgress = 0.0f;
-	private Color focusBorderColor = new Color(255, 255, 255);
-	private Color errorBorderColor = new Color(255, 0, 0);
-	private boolean isErrorState = false;
 
+	private boolean hoveringLeftButton;
+	private boolean hoveringRightButton;
+	
 	public EnumComponent(IGuiElement parent, EnumSetting<T> enumSetting) {
-		super(parent, new Rectangle(null, null, null, 30f));
+		super(parent, new Rectangle(null, null, null, 60f));
 		this.enumSetting = enumSetting;
 
 		this.setMargin(new Margin(8f, 2f, 8f, 2f));
-	}
-
-	@Override
-	public void onVisibilityChanged() {
-		super.onVisibilityChanged();
-		
-		if (this.isVisible())
-			Aoba.getInstance().eventManager.AddListener(KeyDownListener.class, this);
-		else
-			Aoba.getInstance().eventManager.RemoveListener(KeyDownListener.class, this);
 	}
 
 	@Override
@@ -52,30 +38,20 @@ public class EnumComponent<T extends Enum<T>> extends Component implements KeyDo
 	public void draw(DrawContext drawContext, float partialTicks) {
 		super.draw(drawContext, partialTicks);
 
-		MatrixStack matrixStack = drawContext.getMatrices();
-		Matrix4f matrix4f = matrixStack.peek().getPositionMatrix();
-
 		float actualX = actualSize.getX();
 		float actualY = actualSize.getY();
 		float actualWidth = actualSize.getWidth();
-		float actualHeight = actualSize.getHeight();
-
-		if (isFocused) {
-			focusAnimationProgress = Math.min(1.0f, focusAnimationProgress + partialTicks * 0.1f);
-		} else {
-			focusAnimationProgress = Math.max(0.0f, focusAnimationProgress - partialTicks * 0.1f);
-		}
-
-		Color borderColor = isErrorState ? errorBorderColor
-				: new Color(115 + (int) (140 * focusAnimationProgress), 115, 115, 200);
 
 		Render2D.drawString(drawContext, enumSetting.displayName, actualX, actualY + 8, 0xFFFFFF);
 		
-		// Right Text
-		Render2D.drawOutlinedRoundedBox(matrix4f, actualX + actualWidth - 150, actualY, 150, actualHeight, 3.0f, borderColor, new Color(115, 115, 115, 200));
-
+		// Left Arrow and Right Arrow
+		Render2D.drawString(drawContext, "<", actualX + 4, actualY + 34, hoveringLeftButton ? GuiManager.foregroundColor.getValue().getColorAsInt() : 0xFFFFFF);
+		Render2D.drawString(drawContext, ">", actualX + actualWidth - 8, actualY + 34, hoveringRightButton ? GuiManager.foregroundColor.getValue().getColorAsInt() : 0xFFFFFF);
+		
+		// Text
 		String enumValue = this.enumSetting.getValue().toString();
-		Render2D.drawString(drawContext, enumValue, actualX + actualWidth - 145, actualY + 8, 0xFFFFFF);
+		float stringWidth = Render2D.getStringWidth(enumValue);
+		Render2D.drawString(drawContext, enumValue, actualX + (actualWidth / 2.0f) - stringWidth, actualY + 30, 0xFFFFFF);
 	}
 
 	@Override
@@ -84,43 +60,48 @@ public class EnumComponent<T extends Enum<T>> extends Component implements KeyDo
 
 		if (event.button == MouseButton.LEFT && event.action == MouseAction.DOWN) {
 			if (hovered) {
-				listeningForKey = true;
-				event.cancel();
-			} else {
-				listeningForKey = false;
-			}
-		}
-
-		isFocused = listeningForKey;
-	}
-
-	@Override
-	public void OnKeyDown(KeyDownEvent event) {
-		if (listeningForKey) {
-			int key = event.GetKey();
-
-			if (key == GLFW.GLFW_KEY_ENTER || key == GLFW.GLFW_KEY_ESCAPE) {
-				listeningForKey = false;
-			} else {
 				T currentValue = enumSetting.getValue();
 				T[] enumConstants = currentValue.getDeclaringClass().getEnumConstants();
 				int currentIndex = java.util.Arrays.asList(enumConstants).indexOf(currentValue);
 				int enumCount = enumConstants.length;
-
-				if (key == GLFW.GLFW_KEY_RIGHT) {
-					currentIndex = (currentIndex + 1) % enumCount;
-				} else if (key == GLFW.GLFW_KEY_LEFT) {
+	
+				float actualX = actualSize.getX();
+				float actualY = actualSize.getY();
+				float actualWidth = actualSize.getWidth();
+				
+				Rectangle leftArrowHitbox = new Rectangle(actualX, actualY + 34, 16.0f, 16.0f);
+				Rectangle rightArrowHitbox = new Rectangle(actualX + actualWidth - 18.0f, actualY + 34.0f, 16.0f, 16.0f);
+				if(leftArrowHitbox.intersects((float)event.mouseX, (float)event.mouseY)) 
 					currentIndex = (currentIndex - 1 + enumCount) % enumCount;
-				}
-
+				else if (rightArrowHitbox.intersects((float)event.mouseX, (float)event.mouseY)) 
+					currentIndex = (currentIndex + 1) % enumCount;
+				
 				enumSetting.setValue(enumConstants[currentIndex]);
+				event.cancel();
 			}
-
-			event.cancel();
 		}
 	}
-
-	public void setErrorState(boolean isError) {
-		this.isErrorState = isError;
+	
+	@Override
+	public void onMouseMove(MouseMoveEvent event) {
+		super.onMouseMove(event);
+		
+		float actualX = actualSize.getX();
+		float actualY = actualSize.getY();
+		float actualWidth = actualSize.getWidth();
+		
+		Rectangle leftArrowHitbox = new Rectangle(actualX, actualY + 34, 16.0f, 16.0f);
+		Rectangle rightArrowHitbox = new Rectangle(actualX + actualWidth - 18.0f, actualY + 34.0f, 16.0f, 16.0f);
+		
+		boolean wasHoveringLeftButton = hoveringLeftButton;
+		boolean wasHoveringRightButton = hoveringRightButton;
+		hoveringLeftButton = leftArrowHitbox.intersects((float)event.getX(), (float)event.getY());
+		hoveringRightButton = rightArrowHitbox.intersects((float)event.getX(), (float)event.getY());
+		
+		if(hoveringLeftButton || hoveringRightButton)
+			GuiManager.setCursor(CursorStyle.Click);
+		else if(wasHoveringLeftButton || wasHoveringRightButton){
+			GuiManager.setCursor(CursorStyle.Default);
+		}
 	}
 }
