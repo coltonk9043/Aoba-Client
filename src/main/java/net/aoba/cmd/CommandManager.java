bddfde182a30147221fd8ca0c4f11a2ce94ea8cd
Colton <coltonk9043@yahoo.ca>
2024-09-11 23:18:59 -0400

    Command PR fixes

--- src/main/java/net/aoba/cmd/CommandManager.java
+++ src/main/java/net/aoba/cmd/CommandManager.java
@@ -29,9 +29,13 @@ import net.aoba.settings.types.StringSetting;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-
+import net.aoba.cmd.commands.*;
 import java.lang.reflect.Field;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CommandManager {
     private final Map<String, Command> commands = new HashMap<>();
@@ -39,6 +43,37 @@ public class CommandManager {
 
     public static StringSetting PREFIX = new StringSetting("Prefix", "Prefix", ".aoba");
 
+    // Commands
+    public final CmdAimbot aimbot = new CmdAimbot();
+    public final CmdAutoEat autoeat = new CmdAutoEat();
+    public final CmdChestESP chestesp = new CmdChestESP();
+    public final CmdClickgui clickgui = new CmdClickgui();
+    public final CmdEntityESP entityesp = new CmdEntityESP();
+    public final CmdFastBreak fastbreak = new CmdFastBreak();
+    public final CmdFly fly = new CmdFly();
+    public final CmdFreecam freecam = new CmdFreecam();
+    public final CmdFont font = new CmdFont();
+    public final CmdFullbright fullbright = new CmdFullbright();
+    public final CmdHelp help = new CmdHelp();
+    public final CmdHud hud = new CmdHud();
+    public final CmdItemESP itemesp = new CmdItemESP();
+    public final CmdNoclip noclip = new CmdNoclip();
+    public final CmdNoFall nofall = new CmdNoFall();
+    public final CmdNoSlowdown noslowdown = new CmdNoSlowdown();
+    public final CmdNuker nuker = new CmdNuker();
+    public final CmdPlayerESP playeresp = new CmdPlayerESP();
+    public final CmdPOV pov = new CmdPOV();
+    public final CmdReach reach = new CmdReach();
+    public final CmdSpam spam = new CmdSpam();
+    public final CmdSprint sprint = new CmdSprint();
+    public final CmdStep step = new CmdStep();
+    public final CmdTileBreaker tilebreaker = new CmdTileBreaker();
+    public final CmdTimer timer = new CmdTimer();
+    public final CmdTP tp = new CmdTP();
+    public final CmdTracer tracer = new CmdTracer();
+    public final CmdXRay xray = new CmdXRay();
+    public final CmdHistory history = new CmdHistory();
+    
     public CommandManager(List<IAddon> addons) {
         SettingManager.registerSetting(PREFIX, Aoba.getInstance().settingManager.hiddenContainer);
 
@@ -62,18 +97,22 @@ public class CommandManager {
         }));
     }
 
+    /**  Gets the command object from a syntax. */
     public Command getCommandBySyntax(String syntax) {
         return commands.get(syntax);
     }
 
+    /** Gets all of the Commands currently registered, including ones registered by addons.*/
     public Map<String, Command> getCommands() {
         return commands;
     }
 
+    /** Gets the total number of registered Commands, including ones registered by addons*/
     public int getNumOfCommands() {
         return commands.size();
     }
 
+    /** Runs a command. */
     public void command(String[] commandIn) {
         try {
             commandHistory.add(String.join(" ", commandIn));
@@ -92,10 +131,12 @@ public class CommandManager {
         }
     }
 
+    /** Returns the command history. */
     public List<String> getCommandHistory() {
         return commandHistory;
     }
 
+    /** Prints a message into the Minecraft Chat. */
     public static void sendChatMessage(String message) {
         MinecraftClient mc = MinecraftClient.getInstance();
         if (mc.inGameHud != null) {
@@ -103,4 +144,3 @@ public class CommandManager {
         }
     }
 }
-``
