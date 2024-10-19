5c05b2fc02f7aefed620b4a48a7d7becd6deb56c
L4J <80828850+Logging4J@users.noreply.github.com>
2024-10-18 23:31:19 -0400

    NoPush Setting for AntiKnockback

--- src/main/java/net/aoba/mixin/LivingEntityMixin.java
+++ src/main/java/net/aoba/mixin/LivingEntityMixin.java
@@ -1,6 +1,7 @@
 package net.aoba.mixin;
 
 import net.aoba.Aoba;
+import net.aoba.module.modules.combat.AntiKnockback;
 import net.aoba.module.modules.render.NoRender;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.LivingEntity;
@@ -26,5 +27,11 @@ public abstract class LivingEntityMixin extends EntityMixin {
     private void spawnItemParticles(ItemStack stack, int count, CallbackInfo info) {
         NoRender norender = (NoRender) Aoba.getInstance().moduleManager.norender;
         if (norender.getState() && norender.getNoEatParticles() && stack.getComponents().contains(DataComponentTypes.FOOD)) info.cancel();
+    }
+
+    @Inject(method = "pushAwayFrom", at = @At("HEAD"), cancellable = true)
+    private void hookNoPush(CallbackInfo info) {
+        AntiKnockback antiKnockback = Aoba.getInstance().moduleManager.antiknockback;
+        if (antiKnockback.getState() && antiKnockback.getNoPush()) info.cancel();
     }
 }
