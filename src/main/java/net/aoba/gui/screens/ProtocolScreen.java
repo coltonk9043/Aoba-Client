e434651cf2a6257e372153f4bdfe0644a392258c
Donalp012 <26140070@plantelazteca.edu.mx>
2026-06-03 11:32:11 -0600

    Gui overhaul and new things

--- src/main/java/net/aoba/gui/screens/ProtocolScreen.java
+++ src/main/java/net/aoba/gui/screens/ProtocolScreen.java
@@ -9,7 +9,6 @@
 package net.aoba.gui.screens;
 
 import org.apache.commons.lang3.math.NumberUtils;
-
 import net.aoba.managers.ProtocolManager;
 import net.aoba.gui.components.widgets.AobaButtonWidget;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
@@ -34,34 +33,9 @@ public class ProtocolScreen extends Screen {
     @Override
     protected void init() {
         super.init();
-        EditBox line1Widget = new EditBox(this.font, this.width / 2 - 220, 10, 1000, 18, Component.empty());
-        line1Widget.setValue("NOTE: Protocol may bug on some");
-        line1Widget.setBordered(false); line1Widget.setEditable(false); line1Widget.setTextColor(0xFFFF5555);
-        this.addRenderableWidget(line1Widget);
-
-        EditBox line2Widget = new EditBox(this.font, this.width / 2 - 220, 22, 1000, 18, Component.empty());
-        line2Widget.setValue("ViaVersion servers");
-        line2Widget.setBordered(false); line2Widget.setEditable(false); line2Widget.setTextColor(0xFF55FF55);
-        this.addRenderableWidget(line2Widget);
-
-        EditBox vLabel = new EditBox(this.font, this.width / 2 - 180, this.height / 2 - 55, 75, 18, Component.empty());
-        vLabel.setValue("Version:"); vLabel.setBordered(false); vLabel.setEditable(false); vLabel.setTextColor(0xFFAAAAAA);
-        this.addRenderableWidget(vLabel);
-
-        EditBox pLabel = new EditBox(this.font, this.width / 2 - 180, this.height / 2 - 30, 75, 18, Component.empty());
-        pLabel.setValue("Protocol:"); pLabel.setBordered(false); pLabel.setEditable(false); pLabel.setTextColor(0xFFAAAAAA);
-        this.addRenderableWidget(pLabel);
-
-        EditBox pkLabel = new EditBox(this.font, this.width / 2 - 180, this.height / 2 - 5, 75, 18, Component.empty());
-        pkLabel.setValue("Pack Ver:"); pkLabel.setBordered(false); pkLabel.setEditable(false); pkLabel.setTextColor(0xFFAAAAAA);
-        this.addRenderableWidget(pkLabel);
-
-        EditBox bLabel = new EditBox(this.font, this.width / 2 - 180, this.height / 2 + 20, 75, 18, Component.empty());
-        bLabel.setValue("Brand:"); bLabel.setBordered(false); bLabel.setEditable(false); bLabel.setTextColor(0xFFAAAAAA);
-        this.addRenderableWidget(bLabel);
 
         this.versionField = this.addRenderableWidget(new EditBox(this.font, this.width / 2 - 98, this.height / 2 - 60, 196, 18, Component.empty()));
-        this.versionField.setValue(ProtocolManager.getVersionName());
+        this.versionField.setValue(ProtocolManager.OVERRIDE_NAME.isEmpty() ? ProtocolManager.getVersionName() : ProtocolManager.OVERRIDE_NAME);
 
         this.protocolField = this.addRenderableWidget(new EditBox(this.font, this.width / 2 - 98, this.height / 2 - 35, 196, 18, Component.empty()));
         this.protocolField.setValue(Integer.toString(ProtocolManager.getProtocolVersion()));
@@ -72,11 +46,11 @@ public class ProtocolScreen extends Screen {
         this.packVerField.setResponder(text -> this.updateAddButton());
 
         this.brandField = this.addRenderableWidget(new EditBox(this.font, this.width / 2 - 98, this.height / 2 + 15, 80, 18, Component.empty()));
-        this.brandField.setValue(ProtocolManager.getClientBrand());
+        this.brandField.setValue(ProtocolManager.BRAND);
 
         int startX = this.width / 2 - 14;
 
-        AobaButtonWidget vButton = new AobaButtonWidget(startX, this.height / 2 + 14, 18, 20, Component.literal("Va"));
+        AobaButtonWidget vButton = new AobaButtonWidget(startX, this.height / 2 + 14, 18, 20, Component.literal("V"));
         vButton.setPressAction(b -> this.brandField.setValue("vanilla"));
         this.addRenderableWidget(vButton);
 
@@ -88,7 +62,7 @@ public class ProtocolScreen extends Screen {
         foButton.setPressAction(b -> this.brandField.setValue("forge"));
         this.addRenderableWidget(foButton);
 
-        AobaButtonWidget qButton = new AobaButtonWidget(startX + 68, this.height / 2 + 14, 18, 20, Component.literal("Qu"));
+        AobaButtonWidget qButton = new AobaButtonWidget(startX + 68, this.height / 2 + 14, 18, 20, Component.literal("Q"));
         qButton.setPressAction(b -> this.brandField.setValue("quilt"));
         this.addRenderableWidget(qButton);
 
@@ -96,34 +70,101 @@ public class ProtocolScreen extends Screen {
         nfButton.setPressAction(b -> this.brandField.setValue("neoforge"));
         this.addRenderableWidget(nfButton);
 
-        AobaButtonWidget lButton = new AobaButtonWidget(startX + 112, this.height / 2 + 14, 18, 20, Component.literal("Li"));
+        AobaButtonWidget lButton = new AobaButtonWidget(startX + 112, this.height / 2 + 14, 18, 20, Component.literal("L"));
         lButton.setPressAction(b -> this.brandField.setValue("liteloader"));
         this.addRenderableWidget(lButton);
-// neoforge liteloader and quilit added
-// json for register the changes i tried to do with xml but ide say me stackoverflow 
-        this.addButton = new AobaButtonWidget(this.width / 2 - 100, this.height / 2 + 50, 196, 20, Component.literal("Done"));
-        this.addButton.setPressAction(button -> {
-            ProtocolManager.OVERRIDE_NAME = this.versionField.getValue();
-            ProtocolManager.OVERRIDE_PROTOCOL = Integer.parseInt(this.protocolField.getValue());
-            ProtocolManager.OVERRIDE_PACK_VERSION = Integer.parseInt(this.packVerField.getValue());
-            ProtocolManager.BRAND = this.brandField.getValue();
 
+        AobaButtonWidget spoofToggle = new AobaButtonWidget(this.width / 2 - 100, this.height / 2 + 40, 96, 20, Component.literal("Spoof: " + (ProtocolManager.SPOOF_ENABLED ? "ON" : "OFF")));
+        spoofToggle.setPressAction(button -> {
+            this.syncFieldsToManager();
+            ProtocolManager.SPOOF_ENABLED = !ProtocolManager.SPOOF_ENABLED;
+            ProtocolManager.save();
+            this.rebuildWidgets();
+        });
+        this.addRenderableWidget(spoofToggle);
+
+        AobaButtonWidget telemetryToggle = new AobaButtonWidget(this.width / 2 + 4, this.height / 2 + 40, 96, 20, Component.literal("Telemetry: " + (ProtocolManager.BLOCK_TELEMETRY ? "BLOCK" : "ALLOW")));
+        telemetryToggle.setPressAction(button -> {
+            this.syncFieldsToManager();
+            ProtocolManager.BLOCK_TELEMETRY = !ProtocolManager.BLOCK_TELEMETRY;
+            ProtocolManager.save();
+            this.rebuildWidgets();
+        });
+        this.addRenderableWidget(telemetryToggle);
+
+        // --- FILA DE EXPLOIT PROTECTION ---
+        AobaButtonWidget stripToggle = new AobaButtonWidget(this.width / 2 - 100, this.height / 2 + 63, 63, 20, Component.literal("Strip: " + (ProtocolManager.STRIP_KNOWN_PACKS ? "ON" : "OFF")));
+        stripToggle.setPressAction(button -> {
+            this.syncFieldsToManager();
+            ProtocolManager.STRIP_KNOWN_PACKS = !ProtocolManager.STRIP_KNOWN_PACKS;
+            ProtocolManager.save();
+            this.rebuildWidgets();
+        });
+        this.addRenderableWidget(stripToggle);
+
+        AobaButtonWidget scanToggle = new AobaButtonWidget(this.width / 2 - 34, this.height / 2 + 63, 65, 20, Component.literal("AntiScan: " + (ProtocolManager.BLOCK_LOCAL_SCAN ? "ON" : "OFF")));
+        scanToggle.setPressAction(button -> {
+            this.syncFieldsToManager();
+            ProtocolManager.BLOCK_LOCAL_SCAN = !ProtocolManager.BLOCK_LOCAL_SCAN;
+            ProtocolManager.save();
+            this.rebuildWidgets();
+        });
+        this.addRenderableWidget(scanToggle);
+
+        AobaButtonWidget probeToggle = new AobaButtonWidget(this.width / 2 + 34, this.height / 2 + 63, 66, 20, Component.literal("Probes: " + (ProtocolManager.PROBES_PROTECTION ? "ON" : "OFF")));
+        probeToggle.setPressAction(button -> {
+            this.syncFieldsToManager();
+            ProtocolManager.PROBES_PROTECTION = !ProtocolManager.PROBES_PROTECTION;
+            ProtocolManager.save();
+            this.rebuildWidgets();
+        });
+        this.addRenderableWidget(probeToggle);
+
+        this.addButton = new AobaButtonWidget(this.width / 2 - 100, this.height / 2 + 88, 196, 20, Component.literal("Done"));
+        this.addButton.setPressAction(button -> {
+            this.syncFieldsToManager();
             ProtocolManager.save();
             this.onClose();
         });
         this.addRenderableWidget(this.addButton);
 
-        // Botón Cancelar
-        AobaButtonWidget cancelButton = new AobaButtonWidget(this.width / 2 - 100, this.height / 2 + 73, 196, 20, Component.literal("Cancel"));
+        AobaButtonWidget cancelButton = new AobaButtonWidget(this.width / 2 - 100, this.height / 2 + 111, 196, 20, Component.literal("Cancel"));
         cancelButton.setPressAction(button -> this.onClose());
         this.addRenderableWidget(cancelButton);
 
         this.updateAddButton();
     }
 
+    private void syncFieldsToManager() {
+        ProtocolManager.OVERRIDE_NAME = this.versionField.getValue();
+        if (NumberUtils.isDigits(this.protocolField.getValue())) {
+            ProtocolManager.OVERRIDE_PROTOCOL = Integer.parseInt(this.protocolField.getValue());
+        }
+        if (NumberUtils.isDigits(this.packVerField.getValue())) {
+            ProtocolManager.OVERRIDE_PACK_VERSION = Integer.parseInt(this.packVerField.getValue());
+        }
+        ProtocolManager.BRAND = this.brandField.getValue();
+    }
+
     @Override
     public void extractRenderState(GuiGraphicsExtractor drawContext, int mouseX, int mouseY, float delta) {
         super.extractRenderState(drawContext, mouseX, mouseY, delta);
+
+        drawContext.text(this.font, "NOTE: Protocol may bug on some Version servers", this.width / 2 - 220, 10, 0xFFFF5555);
+        drawContext.text(this.font, "WARNING: May cause crashes or anti-cheat bans on strict servers!", this.width / 2 - 220, 20, 0xFFFF5555);
+
+
+        drawContext.text(this.font, "Version:", this.width / 2 - 180, this.height / 2 - 55, 0xFFAAAAAA);
+        drawContext.text(this.font, "Protocol:", this.width / 2 - 180, this.height / 2 - 30, 0xFFAAAAAA);
+        drawContext.text(this.font, "Pack Ver:", this.width / 2 - 180, this.height / 2 - 5, 0xFFAAAAAA);
+        drawContext.text(this.font, "Brand:", this.width / 2 - 180, this.height / 2 + 20, 0xFFAAAAAA);
+
+        int infoY = this.height - 65;
+        drawContext.text(this.font, "Spoof: Sends fake client version/brand to the server.", 15, infoY, 0xFFFF00FF);
+        drawContext.text(this.font, "Telemetry: Blocks background data logs sent to Mojang.", 15, infoY + 10, 0xFFFF00FF);
+        drawContext.text(this.font, "Strip: Removes mod signatures to appear completely Vanilla.", 15, infoY + 20, 0xFFFF00FF);
+        drawContext.text(this.font, "AntiScan: Blocks resource pack exploits targeting local ports.", 15, infoY + 30, 0xFFFF00FF);
+        drawContext.text(this.font, "Probes: Prevents chat exploits used to detect active hacks.", 15, infoY + 40, 0xFFFF00FF);
     }
 
     @Override
